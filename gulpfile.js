34435e4f4226e0547901f1c062c450f468c0bcce
Shika <loka.eslam@hotmail.com>
2019-06-05 08:35:14 +0200

    Providing webpack to bundle beside gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,14 @@ const sass = require("gulp-sass");
 const server = require("gulp-server-livereload");
 const babel = require("gulp-babel");
 const uglify = require("gulp-uglify");
-const concat = require("gulp-concat");
 const autoprefixer = require("gulp-autoprefixer");
-const rename = require("gulp-rename");
 const cleanCss = require("gulp-clean-css");
 
 
-// const build = gulp.series(buildJs, buildCss, buildHtml);
+const build = gulp.series(buildJs, buildCss, buildHtml, buildImages, buildFonts);
 
 gulp.task("watch", function () {
-    return gulp.src("./src")
+    return gulp.src("./dist")
         .pipe(server({
             livereload: true,
             directoryListening: true
@@ -23,15 +21,11 @@ gulp.task('watchScss', function () {
     gulp.watch(['./src/scss', './src/css/owl.theme.default.min.css'], compileSass)
 });
 
-// gulp.task("build", build);
-
-gulp.task("compressJs", compressJs);
+gulp.task("build", build);
 
 gulp.task("buildCss", buildCss);
 
 gulp.task("buildHtml", buildHtml);
-
-gulp.task("minimizeJquery", minimizeJquery);
 
 function compileSass() {
     return gulp.src("./src/scss/main.scss")
@@ -40,23 +34,6 @@ function compileSass() {
             outputStyle: "compressed"
         }))
         .pipe(gulp.dest("./src/css"))
-}
-
-function minimizeJquery() {
-    return gulp.src(["./src/js/jquery.min.js", "./src/js/jquery.easing.compatibility.js", "./src/js/jquery.easing.js", "./src/js/jquery.magnific-popup.min.js"])
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(concat("jquery.conc.js"))
-        .pipe(gulp.dest("./dist/js"))
-}
-
-function compressJs() {
-    // return gulp.src(["./src/js/popper.min.js", "./src/js/isotope.min.js", "./src/js/owl.carousel.min.js", "./src/js/bootstrap.min.js", "./src/js/main.js"])
-    //     .pipe(gulp.dest("./dist/js"))
-    return gulp.src("./src/js/*.js")
-        .pipe(uglify())
-        .pipe(gulp.dest("./dist/js"))
 }
 
 function buildCss() {
@@ -71,10 +48,20 @@ function buildCss() {
 function buildHtml() {
     return gulp.src("./src/index.html")
         .pipe(gulp.dest("./dist"))
-        .pipe(gulp.src("./src/img/*.jpg"))
+}
+
+function buildImages() {
+    return gulp.src("./src/img/*")
         .pipe(gulp.dest("./dist/img"))
-        .pipe(gulp.src("./src/fonts/*"))
+}
+
+function buildFonts() {
+    return gulp.src("./src/fonts/*")
         .pipe(gulp.dest("./dist/fonts"))
-        .pipe(gulp.src("./src/mobirise/*"))
-        .pipe(gulp.dest("./dist/mobirise"))
+}
+
+function buildJs() {
+    return gulp.src(["./src/js/*.js", "!./src/js/typedjs", "!./src/js/main.js"])
+        .pipe(uglify())
+        .pipe(gulp.dest("./dist/js"))
 }
